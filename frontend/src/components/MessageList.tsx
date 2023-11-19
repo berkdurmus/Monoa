9f776c84ecd5828d2377916bc57ef2bf034b3e52
Berk Durmus <berkdurmus@yahoo.com>
2023-11-19 22:36:42 +0300

    chore(styles): add tailwind to MesageList.tsx

--- frontend/src/components/MessageList.tsx
+++ frontend/src/components/MessageList.tsx
@@ -6,9 +6,9 @@ const MessageList: React.FC = () => {
   const messages = useMessages();
 
   return (
-    <div>
+    <div className="bg-gray-100 p-4 rounded">
       {messages.map((msg, index) => (
-        <p key={index}>{msg.content}</p>
+        <p key={index} className="p-2 bg-white my-2 rounded shadow">{msg.content}</p>
       ))}
     </div>
   );
